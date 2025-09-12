a1800df3ed43e2e19c5714e22bbc8d34c2edab11
zdjts <z15188247@gmail.com>
2025-09-13 00:09:34 +0800

    feat: 添加新的插件，修改treesister.lua - 引入`rainbow_delimiters`插件，为括号提供分层着色。 - 更新`treesister`与`theme`以支持新的高亮设置，并提供了变量和属性添加自定义颜色。

--- lua/code/treesister.lua
+++ lua/code/treesister.lua
@@ -1,33 +1,57 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
+  branch = "master",
   lazy = false,
   build = ":TSUpdate",
 
   -- 解释器列表
   ensure_installed = {
-    'bash',
-    'python',
-    'cpp',
-    'diff',
-    'html',
-    'xml',
-    'lua',
-    'luadoc',
-    'markdown',
-    'markdown_inline',
-    'query',
-    'vim',
-    'vimdoc',
-    'snakemake',
+    "bash",
+    "python",
+    "cpp",
+    "diff",
+    "html",
+    "xml",
+    "lua",
+    "luadoc",
+    "markdown",
+    "markdown_inline",
+    "query",
+    "vim",
+    "vimdoc",
+    "snakemake",
   },
   ignore_install = {
-    'latex',
+    "latex",
   },
   auto_install = true,
-  highlight = {
-    enable = true,
-    disable = { 'latex' },
-    additional_vim_regex_highlighting = { 'ruby' },
-  },
+
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup {
+      highlight = {
+        enable = true,
+        disable = { "latex" },
+        additional_vim_regex_highlighting = { "ruby" },
+      },
+    }
+
+    -- 将 @variable 设置为特定的颜色
+    -- 设置 Treesitter 的变量高亮。@variable 通常指代函数体内的局部变量。
+    -- fg: 前景颜色（文本颜色），bg: 背景颜色，bold: 字体加粗。
+    -- vim.api.nvim_set_hl(0, "@variable", { fg = "#FF00FF", bg = "NONE", bold = true })
+    --
+    -- -- 设置 Treesitter 的属性高亮。@property 通常指代对象属性，如 my_object.property。
+    -- -- 这里只修改前景色和字体加粗。
+    -- vim.api.nvim_set_hl(0, "@property", { fg = "#FFD700", bold = true })
+    --
+    -- -- 设置 Treesitter 的变量成员高亮。@variable.member 更加精确，用于嵌套的成员，如 my_object.property.value。
+    -- vim.api.nvim_set_hl(0, "@variable.member", { fg = "#1E90FF", bold = true })
+    --
+    -- -- 设置 Treesitter 的所有括号高亮。@punctuation.bracket 是一个通用组，用于所有括号类型。
+    -- vim.api.nvim_set_hl(0, "@punctuation.bracket", { fg = "#FFFF00", bold = true })
+
+    -- 设置所有括号相关（这是你提供的代码，作用是让括号加粗，但不改变颜色）
+    -- 这个设置会覆盖上面 @punctuation.bracket 的颜色设置，只保留 bold。
+    vim.api.nvim_set_hl(0, "@punctuation.bracket", { bold = true })
+  end,
 }
--- lua/ui/theme.lua
+++ lua/ui/theme.lua
@@ -1,13 +1,19 @@
 return {
   {
-"catppuccin/nvim",
-name = "catppuccin",
-priority = 1000,
-opts = {
-	flavour = "mocha",
-},
-config = function () 
-	vim.cmd.colorscheme("catppuccin")
-end
-},
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",   -- 你要使用的颜色主题
+      integrations = {
+        treesitter = true, -- 确保 Treesitter 集成开启
+      },
+    },
+    config = function(_, opts)
+      -- 1. 使用 setup() 函数将 opts 表中的配置应用到插件
+      require("catppuccin").setup(opts)
+      -- 2. 使用 colorscheme 命令来真正应用这个主题
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
 }
