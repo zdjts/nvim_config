c6ae3ae7ad3291cd4bf1a7c8be2e19c30c234a3e
zdjts <z15188247@gmail.com>
2025-09-27 13:34:57 +0800

    fix(code): 修正treesister插件的启动阻塞问题 这次提交通过启用插件的懒加载(lazy = true)来解决neovim提前加载导致的阻塞问题。
    - 将treesister中lazy修改为true。
    - 修复了由于插件过早加载导致的启动阻塞问题

--- lua/code/treesister.lua
+++ lua/code/treesister.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'master',
-  lazy = false,
+  lazy = true,
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
