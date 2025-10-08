563b127c7d986123d1f2bf902c255a2fb28a4b56
zdjts <z15188247@gmail.com>
2025-10-08 13:09:56 +0800

    feat(LSP): 新增CPP诊断功能
    - clangd调用clangd-tidy提供诊断功能
    
    修改了 lsp/clangd.lua 文件，新增CPP语言的诊断功能支持，通过clangd-tidy实现代码质量检查和静态分析。

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,5 +1,9 @@
 return {
-  cmd = { 'clangd' },
+  cmd = {
+    'clangd',
+    '--background-index',
+    '--clang-tidy',
+  },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
   root_markers = {
     '.clangd',
