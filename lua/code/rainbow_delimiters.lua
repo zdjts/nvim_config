e553d27ac3f2a7c3fb296d60cf2efbf22178a1ba
zdjts <z15188247@gmail.com>
2025-10-19 20:24:47 +0800

    fix: 修复rainbow插件不加载问题。

--- lua/code/rainbow_delimiters.lua
+++ lua/code/rainbow_delimiters.lua
@@ -1,6 +1,8 @@
 -- 安装 rainbow-delimiters 插件
 return {
   'hiphish/rainbow-delimiters.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   -- 你不需要任何额外的配置，它开箱即用
   -- 它会与你的 colorscheme 完美配合
 }
